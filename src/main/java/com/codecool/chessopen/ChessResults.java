1f021c7a6fc19121efd969dd66bc3393852bd0b0
DurayBence <duray.bence@users.noreply.github.com>
2021-03-24 15:51:08 +0100

    Remove unused imports

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -5,10 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class ChessResults {
 
